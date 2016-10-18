7ca445639abd7ef3fc665b0d0f5d564843d1a010
Ferdestroyer <ferdestroyer@gmail.com>
2016-10-18 19:21:54 +0200

    Empiece de ejercicio 9

---
+++ miprimerprograma/src/codigoprincipal/Ejercicio9.java
@@ -0,0 +1,19 @@
+package codigoprincipal;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		int numero1=0;
+		int numero2=0;
+		
+		System.out.println ("Introduce el primer numero:"); //texto que indica que escribamos el numero
+		Scanner numero = new Scanner(System.in); //nos guarda lo que escribamos por teclado
+
+	       numero1=numero.nextInt(); //decimos que el numero 1 sea = a lo que ponemos por teclado
+		
+
+	}
+
+}
