3f75fe2d1da8027b9123feeb3f7a0795040cd66e
Ferdestroyer <ferdestroyer@gmail.com>
2016-10-20 21:07:22 +0200

    Empiece de ejercicio 10

---
+++ miprimerprograma/src/codigoprincipal/Ejercicio10.java
@@ -0,0 +1,23 @@
+package codigoprincipal;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		
+		int a=0;
+		int b=0;
+		
+		
+		
+		
+		
+		while (a==0){
+			
+			
+		}
+		
+		
+
+	}
+
+}
