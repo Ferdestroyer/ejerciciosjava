561368c4d3c5ec9dcf86fb52b92dad00fa147a79
Ferdestroyer <ferdestroyer@gmail.com>
2016-10-20 21:06:53 +0200

    Ejercicio 9 terminado y perfecionado

--- miprimerprograma/src/codigoprincipal/Ejercicio9.java
+++ miprimerprograma/src/codigoprincipal/Ejercicio9.java
@@ -8,11 +8,22 @@ public class Ejercicio9 {
 		int numero1=0;
 		int numero2=0;
 		
+		
 		System.out.println ("Introduce el primer numero:"); //texto que indica que escribamos el numero
 		Scanner numero = new Scanner(System.in); //nos guarda lo que escribamos por teclado
 
 	       numero1=numero.nextInt(); //decimos que el numero 1 sea = a lo que ponemos por teclado
-	       System.out.println ("estoy desde casa");	
+	      
+	       numero2=(numero1%2); //decimos que numero2 es = al resto de numero1 entre 2
+	       
+	       if (numero2==0){		//si el resto es = a 0
+	    	   
+	    	   System.out.println (+numero1+" es par");		//el numero es par
+	       }
+	       else{									
+	    	   
+	    	   System.out.println (+numero1+" es impar");	//el numero es impar
+	       }
 	       
 	}
 
