a9dbebe4d5f8a9ebd2f010fcd562f921983258e7
Ferdestroyer <ferdestroyer@gmail.com>
2016-11-24 21:05:49 +0100

    Ejercicio 1 finalizado, Ejercicio 2 en proceso

--- miprimerprograma/src/ejerciciohoja3/Ejercicio2.java
+++ miprimerprograma/src/ejerciciohoja3/Ejercicio2.java
@@ -2,28 +2,28 @@ package ejerciciohoja3;
 
 import java.util.Scanner;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
 
 	public static void main(String[] args) {
 
 	String frase="";
 	int contador;
-	contador=1;
-	String frasefinal="";
+	contador=0;
+	int longitud=0;
 	Scanner teclado = new Scanner(System.in);
-
+	
 	System.out.println("introduce la frase");
 		frase=teclado.nextLine();
 		
-		int longitud;
-		longitud=frase.length();
+		int mayus;
+		mayus=frase.length();
 		do{
-		
-			frasefinal=frase.substring(0,contador);
+			longitud=frase.indexOf(contador);
+			char letra=frase.charAt(contador);
 			contador=contador+1;
-			System.out.println(frasefinal);
-			
-		}while (contador<=longitud);
+			if (letra)
+		
+		}while (contador<=10);
 		
 		
 		
