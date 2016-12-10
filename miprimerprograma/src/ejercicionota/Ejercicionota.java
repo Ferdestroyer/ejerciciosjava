f6d8cdfa27e8fa5e0a5f45e073bd5698a819a491
Ferdestroyer <Ferdestroyer@gmail.com>
2016-12-10 18:37:52 +0100

    Fallos corregidos en el if

--- miprimerprograma/src/ejercicionota/Ejercicionota.java
+++ miprimerprograma/src/ejercicionota/Ejercicionota.java
@@ -34,14 +34,14 @@ public class Ejercicionota {
 			int pos2b=valor2.indexOf('.');
 			int pos3b=valor2.indexOf('/');
 			int pos4b=valor2.indexOf(':');
-			int pos1a1=valor1.indexOf('/', pos1a+1);
-			int pos2a1=valor1.indexOf('/', pos2a+1);
+			int pos1a1=valor1.indexOf('-', pos1a+1);
+			int pos2a1=valor1.indexOf('.', pos2a+1);
 			int pos3a1=valor1.indexOf('/', pos3a+1);
-			int pos4a1=valor1.indexOf('/', pos4a+1);
-			int pos1b1=valor1.indexOf('/', pos1b+1);
-			int pos2b1=valor1.indexOf('/', pos2b+1);
-			int pos3b1=valor1.indexOf('/', pos3b+1);
-			int pos4b1=valor1.indexOf('/', pos4b+1);
+			int pos4a1=valor1.indexOf(':', pos4a+1);
+			int pos1b1=valor2.indexOf('-', pos1b+1);
+			int pos2b1=valor2.indexOf('.', pos2b+1);
+			int pos3b1=valor2.indexOf('/', pos3b+1);
+			int pos4b1=valor2.indexOf(':', pos4b+1);
 			//El if definitivo, el pilar de la creación
 			if(((pos1a==2 && pos1a1==5) || (pos1a==2 && pos1a1==4) || (pos2a==2 && pos2a1==5) || (pos2a==2 && pos2a1==4) || (pos3a==2 && pos3a1==5) || (pos3a==2 && pos3a1==4) || (pos4a==2 && pos4a1==5) || (pos4a==2 && pos4a1==4) || (pos1a==1 && pos1a1==3) || (pos1a==1 && pos1a1==4) || (pos2a==1 && pos2a1==3) || (pos2a==1 && pos2a1==4) || (pos3a==1 && pos3a1==3) || (pos3a==1 && pos3a1==4) || (pos4a==1 && pos4a1==3) || (pos4a==1 && pos4a1==4)) && ((pos1b==2 && pos1b1==5) || (pos1b==2 && pos1b1==4) || (pos2b==2 && pos2b1==5) || (pos2b==2 && pos2b1==4) || (pos3b==2 && pos3b1==5) || (pos3b==2 && pos3b1==4) || (pos4b==2 && pos4b1==5) || (pos4b==2 && pos4b1==4) || (pos1b==1 && pos1b1==3) || (pos1b==1 && pos1b1==4) || (pos2b==1 && pos2b1==3) || (pos2b==1 && pos2b1==4) || (pos3b==1 && pos3b1==3) || (pos3b==1 && pos3b1==4) || (pos4b==1 && pos4b1==3) || (pos4b==1 && pos4b1==4))){
 				System.out.println("exito");
