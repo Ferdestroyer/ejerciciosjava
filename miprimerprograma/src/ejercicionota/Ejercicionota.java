801f64ab765feaf7c3fbda634e29a06a1c29e56c
Ferdestroyer <Ferdestroyer@gmail.com>
2016-12-10 19:25:52 +0100

    agregada segunda condición + comentarios

--- miprimerprograma/src/ejercicionota/Ejercicionota.java
+++ miprimerprograma/src/ejercicionota/Ejercicionota.java
@@ -15,37 +15,46 @@ public class Ejercicionota {
 
 	public static void main(String[] args) {
 		
-		String valor1="";
-		String valor2="";
-		Scanner teclado = new Scanner(System.in);
+		String valor1=""; //variable "valor1" de tipo cadena con valor NULL
+		String valor2=""; //variable "valor2" de tipo cadena con valor NULL
+		Scanner teclado = new Scanner(System.in); //Llamada funcion coger valor por teclado
+		System.out.println("Introduce una fecha o un numero entero"); //Imprimir por pantalla: "Introduce una fecha o un numero entero"
+		valor1=teclado.nextLine(); //Pedir valor por teclado y guardarlo en "valor1"
 		System.out.println("Introduce una fecha o un numero entero");
-		valor1=teclado.nextLine();	
-		System.out.println("Introduce una fecha o un numero entero");
-		valor2=teclado.nextLine();	
-		int longitud1=valor1.length();
-		int longitud2=valor2.length();
+		valor2=teclado.nextLine();	//Pedir valor por teclado y guardarlo en "valor2"
+		int longitud1=valor1.length(); //leemos la longitud de valor1 y la guardamos en longitud1
+		int longitud2=valor2.length(); //leemos la longitud de valor 2 y la guardamos en longitud2
 		
-		if (longitud1<=10 && longitud2<=10){
-			int pos1a=valor1.indexOf('-');
-			int pos2a=valor1.indexOf('.');
-			int pos3a=valor1.indexOf('/');
-			int pos4a=valor1.indexOf(':');
-			int pos1b=valor2.indexOf('-');
-			int pos2b=valor2.indexOf('.');
-			int pos3b=valor2.indexOf('/');
-			int pos4b=valor2.indexOf(':');
-			int pos1a1=valor1.indexOf('-', pos1a+1);
-			int pos2a1=valor1.indexOf('.', pos2a+1);
-			int pos3a1=valor1.indexOf('/', pos3a+1);
-			int pos4a1=valor1.indexOf(':', pos4a+1);
-			int pos1b1=valor2.indexOf('-', pos1b+1);
-			int pos2b1=valor2.indexOf('.', pos2b+1);
-			int pos3b1=valor2.indexOf('/', pos3b+1);
-			int pos4b1=valor2.indexOf(':', pos4b+1);
-			//El if definitivo, el pilar de la creación
+		if (longitud1<=10 && longitud2<=10){ //condicion en que la longitud de ambos debe ser <=10 
+			int pos1a=valor1.indexOf('-');	//buscar el primer valor "-" de la cadena 1 y guardar la posicion en pos1a
+			int pos2a=valor1.indexOf('.');  //buscar el primer valor "." de la cadena 1 y guardar la posicion en pos2a
+			int pos3a=valor1.indexOf('/');  //buscar el primer valor "/" de la cadena 1 y guardar la posicion en pos3a
+			int pos4a=valor1.indexOf(':');  //buscar el primer valor ":" de la cadena 1 y guardar la posicion en pos4a
+			int pos1b=valor2.indexOf('-');  //buscar el primer valor "-" de la cadena 2 y guardar la posicion en pos1b
+			int pos2b=valor2.indexOf('.');  //buscar el primer valor "." de la cadena 2 y guardar la posicion en pos2b
+			int pos3b=valor2.indexOf('/');  //buscar el primer valor "/" de la cadena 2 y guardar la posicion en pos3b
+			int pos4b=valor2.indexOf(':');  //buscar el primer valor ":" de la cadena 2 y guardar la posicion en pos4b
+			int pos1a1=valor1.indexOf('-', pos1a+1); //buscar el siguiente valor "-" partiendo de pos1a+1 de la cadena 1 y guardar la posicion en pos1a1
+			int pos2a1=valor1.indexOf('.', pos2a+1); //buscar el siguiente valor "." partiendo de pos2a+1 de la cadena 1 y guardar la posicion en pos2a1
+			int pos3a1=valor1.indexOf('/', pos3a+1); //buscar el siguiente valor "/" partiendo de pos3a+1 de la cadena 1 y guardar la posicion en pos3a1
+			int pos4a1=valor1.indexOf(':', pos4a+1); //buscar el siguiente valor ":" partiendo de pos4a+1 de la cadena 1 y guardar la posicion en pos4a1
+			int pos1b1=valor2.indexOf('-', pos1b+1); //buscar el siguiente valor "-" partiendo de pos1b+1 de la cadena 2 y guardar la posicion en pos1b1
+			int pos2b1=valor2.indexOf('.', pos2b+1); //buscar el siguiente valor "." partiendo de pos2b+1 de la cadena 2 y guardar la posicion en pos2b1
+			int pos3b1=valor2.indexOf('/', pos3b+1); //buscar el siguiente valor "/" partiendo de pos3b+1 de la cadena 2 y guardar la posicion en pos3b1
+			int pos4b1=valor2.indexOf(':', pos4b+1); //buscar el siguiente valor ":" partiendo de pos4b+1 de la cadena 2 y guardar la posicion en pos4b1
+			
+			//Condición con operadores lógicos que se encarga de analizar la condición 2 (DDMMAAAA) del ejercicio
+			
+			
 			if(((pos1a==2 && pos1a1==5) || (pos1a==2 && pos1a1==4) || (pos2a==2 && pos2a1==5) || (pos2a==2 && pos2a1==4) || (pos3a==2 && pos3a1==5) || (pos3a==2 && pos3a1==4) || (pos4a==2 && pos4a1==5) || (pos4a==2 && pos4a1==4) || (pos1a==1 && pos1a1==3) || (pos1a==1 && pos1a1==4) || (pos2a==1 && pos2a1==3) || (pos2a==1 && pos2a1==4) || (pos3a==1 && pos3a1==3) || (pos3a==1 && pos3a1==4) || (pos4a==1 && pos4a1==3) || (pos4a==1 && pos4a1==4)) && ((pos1b==2 && pos1b1==5) || (pos1b==2 && pos1b1==4) || (pos2b==2 && pos2b1==5) || (pos2b==2 && pos2b1==4) || (pos3b==2 && pos3b1==5) || (pos3b==2 && pos3b1==4) || (pos4b==2 && pos4b1==5) || (pos4b==2 && pos4b1==4) || (pos1b==1 && pos1b1==3) || (pos1b==1 && pos1b1==4) || (pos2b==1 && pos2b1==3) || (pos2b==1 && pos2b1==4) || (pos3b==1 && pos3b1==3) || (pos3b==1 && pos3b1==4) || (pos4b==1 && pos4b1==3) || (pos4b==1 && pos4b1==4))){
 				System.out.println("exito");
 				
+			}
+			//Condición con operadores lógicos que se encarga de analizar la condición 2 (AAAAMMDD) del ejercicio
+			
+			else if(((pos1a==4 && pos1a1==6) || (pos1a==4 && pos1a1==7) || (pos2a==4 && pos2a1==6) || (pos2a==4 && pos2a1==7) || (pos3a==4 && pos3a1==6) || (pos3a==4 && pos3a1==7) || (pos4a==4 && pos4a1==6) || (pos4a==4 && pos4a1==7)) && ((pos1b==4 && pos1b1==6) || (pos1b==4 && pos1b1==7) || (pos2b==4 && pos2b1==6) || (pos2b==4 && pos2b1==7) || (pos3b==4 && pos3b1==6) || (pos3b==4 && pos3b1==7) || (pos4b==4 && pos4b1==6) || (pos4b==4 && pos4b1==7))){
+				
+				System.out.println("bien");
 			}
 			else{
 				System.out.println("error");
